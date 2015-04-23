34096b7e3a1f99da89c31a7f786b9c6f003840af
Sparkier <alex.baeuerle@uni-ulm.de>
2015-04-23 17:46:06 +0200

    Bluetooth Logo embedded

--- skife/NoBluetoothView.swift
+++ skife/NoBluetoothView.swift
@@ -14,11 +14,17 @@ class NoBluetoothView: UIView {
         let screenSize: CGRect = UIScreen.mainScreen().bounds
         super.init(frame: CGRectMake(0, 0, screenSize.width, screenSize.height))
         self.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.9)
+        
         var nbLabel = UILabel(frame: CGRectMake(0, screenSize.height-30, screenSize.width, 30))
         nbLabel.text = "Turn on your Bluetooth to continue."
         nbLabel.textAlignment = NSTextAlignment.Center
         nbLabel.textColor = UIColor(red: 102.0/255.0, green: 204.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+        
+        var nbView = UIImageView(frame: CGRectMake(10, (screenSize.height/2) - ((screenSize.width - 20)/2), screenSize.width-20, screenSize.width-20))
+        nbView.image = UIImage(named: "Bluetooth")
+        
         self.addSubview(nbLabel)
+        self.addSubview(nbView)
     }
 
     required init(coder aDecoder: NSCoder) {
